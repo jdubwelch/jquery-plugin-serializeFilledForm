2e335368cdc5ee521177c0516ae3dd10d0bd2e89
Jason Welch <jason@jwelchdesign.com>
2013-11-09 12:31:06 -0800

    Form variable cached

---
+++ jquery.serializeFilledForm.js
@@ -0,0 +1,52 @@
+/**
+ * this plugin goes through a form's inputs
+ * it removes any unset fields or disabled fields
+ * then builds the query string
+ * 
+ * Author: Jason Welch
+ * http://www.jwelchdesign.com
+ */
+(function ($) {
+	$.fn.serializeFilledForm = function( options ) {
+
+		// set some default options
+		var settings = $.extend({
+				decode: true, // return params URI decoded
+				invalidClass: null // pass a class to invalidate a field
+			}, options),
+			form = this,
+			clone = this.clone( true , true ),
+			serialized;
+
+		// get the select and textarea values
+		$.each(form.find('select'), function(i) {
+			clone.find('select').eq(i).val( $(this).val() );
+		});
+		$.each(form.find('textarea'), function(i) {
+			clone.find('textarea').eq(i).val( $(this).val() );
+		});
+
+		// filter out the empty fields
+		clone.find('input:text, select, textarea').filter(invalidField).remove();
+
+		// serialize it
+		serialized = clone.serialize(); 
+
+		if (settings.decode)
+			return decodeURI( serialized );
+		else
+			return serialized;
+
+		function invalidField() {
+
+			// input has to have a value
+			if ( ! this.value )
+				return true;
+
+			if (settings.invalidClass && $(this).hasClass( settings.invalidClass ))
+				return true;
+
+			return false;
+		}
+	};
+})( jQuery );
